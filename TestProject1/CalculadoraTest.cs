40da884e29ef49cc87cdccf73a0c7e4b122cbc12
MarceloCRM <marcelo.crmoura@gmail.com>
2024-08-20 20:30:26 -0300

    iniciando testes

---
+++ TestProject1/CalculadoraTest.cs
@@ -0,0 +1,53 @@
+using AulasPOO.uteis;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void SomaTest()
+        {
+            //Cenário
+            var calc = new Calculadora();
+            int num1 = 20;
+            int num2 = 22;
+            int resultado;
+            //Ação
+            resultado = calc.Soma(num1, num2);
+            //Teste
+            Assert.AreEqual(42, resultado);
+        }
+
+        [TestMethod]
+        public void MultiplicaTest()
+        {
+            int num1 = 20;
+            int num2 = 2;
+            int resultado;
+            resultado = Calculadora.Multiplica(num1, num2);
+            Assert.AreEqual(40, resultado);
+        }
+
+        [TestMethod]
+
+        public void FatorialTest()
+        {
+            int num = 5;
+            int resultado;
+            resultado = Calculadora.Fatorial(num);
+            Assert.AreEqual(120, resultado);
+        }
+
+        [TestMethod]
+
+        public void ImcTest()
+        {
+            float peso = 87.5f;
+            float altura = 1.8f;
+            float imc;
+            imc = Calculadora.IMC(peso, altura);
+            Assert.AreEqual(27.00, imc);
+        }
+    }
+}
