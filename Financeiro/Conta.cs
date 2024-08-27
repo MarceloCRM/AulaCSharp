6d80003d2313fed7b32cf17d7b449c0ccab449dd
MarceloCRM <marcelo.crmoura@gmail.com>
2024-08-27 20:54:10 -0300

    Financeiro iniciação a POO

---
+++ Financeiro/Conta.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Financeiro
+{
+    public class Conta
+    {
+        //Atributo
+        private int _numero;
+
+        //Propriedade
+        public int Numero 
+        {
+            get
+            {
+                return _numero;
+            }
+            private set
+            {
+                _numero = value;
+            }
+        }
+        public Conta(int numero)
+        {
+            this._numero = numero;
+        }
+        public Conta()
+        {
+
+        }
+    }
+}
---
+++ Financeiro/Program.cs
@@ -0,0 +1,6 @@
+﻿using Financeiro;
+
+Conta conta1 = new Conta(1);
+Conta conta2 = new Conta();
+Console.WriteLine($"O número da conta é {conta1.Numero}");
+Console.WriteLine($"O número da conta é {conta2.Numero}");
